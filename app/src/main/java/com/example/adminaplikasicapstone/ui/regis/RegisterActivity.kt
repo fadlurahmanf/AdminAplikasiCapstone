d7dac155afd9706bb32fa1c23ef13ec0ea5c5725
taufik <tffajari@gmail.com>
2021-05-31 12:32:33 +0700

    PERBAIKAN LOGIC REGISTER

--- app/src/main/java/com/example/adminaplikasicapstone/ui/regis/RegisterActivity.kt
+++ app/src/main/java/com/example/adminaplikasicapstone/ui/regis/RegisterActivity.kt
@@ -17,6 +17,8 @@ import com.example.adminaplikasicapstone.utils.firestore.FirestoreServices
 class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var email_text:EditText
     private lateinit var password_text:EditText
+    private lateinit var phoneNumber_text:EditText
+    private lateinit var confPassword_text:EditText
     private lateinit var city_text:EditText
     private lateinit var btn_regis:Button
     private lateinit var btn_login:TextView
@@ -29,7 +31,9 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     private fun initializationIdLayout() {
         email_text = findViewById(R.id.registeractivity_email)
         password_text = findViewById(R.id.registeractivity_password)
+        confPassword_text = findViewById(R.id.registeractivity_confpassword)
         city_text = findViewById(R.id.registeractivity_city)
+        phoneNumber_text = findViewById(R.id.registeractivity_phoneNumber)
         btn_regis = findViewById(R.id.registeractivity_btnRegis)
         btn_login = findViewById(R.id.registeractivity_btn_login)
         btn_regis.setOnClickListener(this)
@@ -39,7 +43,13 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.registeractivity_btnRegis->{
-                insertAdminDataToFirestore()
+                checkIsAllTextFieldIsNotEmpty()
+                if (checkIsAllTextFieldIsNotEmpty()){
+                    if (checkPasswordAndConfPasswordIsMatch()){
+                        insertAdminDataToFirestore()
+                    }
+                }
+//                insertAdminDataToFirestore()
             }
             R.id.registeractivity_btn_login->{
                 onBackPressed()
@@ -51,7 +61,12 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
         var authenticationServices = AuthenticationServices()
         var result = authenticationServices.AdminData().signUpAdmin(email_text.text.toString(), password_text.text.toString())
         result.addOnSuccessListener {
-            println("BERHASIL")
+            Toast.makeText(this, "BERHASIL DAFTAR", Toast.LENGTH_LONG).show()
+            email_text.text.clear()
+            city_text.text.clear()
+            phoneNumber_text.text.clear()
+            password_text.text.clear()
+            confPassword_text.text.clear()
         }.addOnFailureListener {
             Toast.makeText(this, "${it.message}", Toast.LENGTH_LONG).show()
         }
@@ -70,5 +85,42 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
         }.addOnFailureListener {
             Toast.makeText(this, "${it.message}", Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun checkIsAllTextFieldIsNotEmpty():Boolean{
+        var isAllTextFieldIsNotEmpty:Boolean = false
+        if (email_text.text.isNullOrEmpty()){
+            email_text.error = "ISI EMAIL"
+            isAllTextFieldIsNotEmpty = false
+        }else if (city_text.text.isNullOrEmpty()){
+            city_text.error = "ISI KOTA"
+            isAllTextFieldIsNotEmpty = false
+        }else if (phoneNumber_text.text.isNullOrEmpty()){
+            phoneNumber_text.error = "ISI NOMOR TELEPON"
+            isAllTextFieldIsNotEmpty = false
+        }else if (password_text.text.isNullOrEmpty()){
+            password_text.error = "ISI PASSWORD"
+            isAllTextFieldIsNotEmpty = false
+        }else if (confPassword_text.text.isNullOrEmpty()){
+            confPassword_text.error = "ISI KONFIRMASI PASSWORD"
+            isAllTextFieldIsNotEmpty = false
+        }else if (email_text.text.isNotEmpty() && city_text.text.isNotEmpty()
+                &&phoneNumber_text.text.isNotEmpty() && password_text.text.isNotEmpty()
+                && confPassword_text.text.isNotEmpty()){
+            isAllTextFieldIsNotEmpty = true
+        }
+        return isAllTextFieldIsNotEmpty
+    }
+
+    private fun checkPasswordAndConfPasswordIsMatch():Boolean{
+        var isMatch:Boolean = false
+        if (password_text.text.toString()==confPassword_text.text.toString()){
+            isMatch = true
+        }else{
+            isMatch = false
+            password_text.error = "PASSWORD DAN KONFIRMASI PASSWORD TIDAK SESUAI"
+            confPassword_text.error = "PASSWORD DAN KONFIRMASI PASSWORD TIDAK SESUAI"
+        }
+        return isMatch
     }
 }
